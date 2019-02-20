cbeb1abf2cb6b91a376a93accfe5546cdf79618a
J A <jarata@gmail.com>
2019-02-20 09:35:30 -0800

    imported and added morgan

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'); // importing a CommonJS module
 const helmet = require('helmet');
+const morgan = require('morgan');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
@@ -7,14 +8,21 @@ const server = express();
 
 server.use(express.json());
 server.use(helmet());
+server.use(morgan('dev'));
 
 server.use('/api/hubs', hubsRouter);
+
+
 
 server.get('/', (req, res, next) => {
   res.send(`
     <h2>Lambda Hubs API</h2>
     <p>Welcome to the Lambda Hubs API</p>
     `);
+});
+
+server.use(function (req, res) {
+  res.status(404).send(`Ain't nobody got time for dat!`)
 });
 
 module.exports = server;
