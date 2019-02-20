20ecc0e8ae58410ac3110b6e485d8de4e92009e4
J A <jarata@gmail.com>
2019-02-20 10:11:49 -0800

    added authorization custom middleware

--- server.js
+++ server.js
@@ -15,19 +15,20 @@ server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
 server.use(teamName);
-server.use(moodyGateKeeper);
+// server.use(moodyGateKeeper);
+server.use(restricted);
 
 server.use('/api/hubs', hubsRouter);
 
-function moodyGateKeeper(req, res, next) {
-  const seconds = new Date().getSeconds();
-
-  if (seconds % 3 === 0) {
-    res.status(403).json('none shall pass')
-  } else {
-    next();
-  }
-}
+// function moodyGateKeeper(req, res, next) {
+//   const seconds = new Date().getSeconds();
+//
+//   if (seconds % 3 === 0) {
+//     res.status(403).json('none shall pass')
+//   } else {
+//     next();
+//   }
+// }
 
 server.get('/', (req, res, next) => {
   res.send(`
@@ -35,6 +36,18 @@ server.get('/', (req, res, next) => {
     <p>Welcome ${req.team} to the Lambda Hubs API</p>
     `);
 });
+
+function restricted(req, res, next) {
+  const password = req.headers.authorization;
+
+  if (password == 'mellon') {
+    next();
+  } else {
+    res.status(401).json({
+      message: 'You are not authorized'
+    })
+  }
+}
 
 // server.use(function (req, res) {
 //   res.status(404).send(`Ain't nobody got time for dat!`)
