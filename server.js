a8991488416a9c71c08b8722b4f4926df5cbf686
J A <jarata@gmail.com>
2019-02-20 09:42:53 -0800

    added custom middleware

--- server.js
+++ server.js
@@ -6,23 +6,27 @@ const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+function teamName(req, res, next) {
+  req.team = 'Lambda Students';
+  next();
+}
+
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
+server.use(teamName);
 
 server.use('/api/hubs', hubsRouter);
-
-
 
 server.get('/', (req, res, next) => {
   res.send(`
     <h2>Lambda Hubs API</h2>
-    <p>Welcome to the Lambda Hubs API</p>
+    <p>Welcome ${req.team} to the Lambda Hubs API</p>
     `);
 });
 
-server.use(function (req, res) {
-  res.status(404).send(`Ain't nobody got time for dat!`)
-});
+// server.use(function (req, res) {
+//   res.status(404).send(`Ain't nobody got time for dat!`)
+// });
 
 module.exports = server;
